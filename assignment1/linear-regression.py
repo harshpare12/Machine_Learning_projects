1fff0b00e98f80e63a92912c17cf462d1ebbecf6
harshpare12 <harshpare12@gmail.com>
2023-08-27 14:03:03 +0530

    Added all Python files for all assignments
    Author: Harsh Pare

---
+++ assignment1/linear-regression.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.animation as animation
+from matplotlib import cm
+import math
+import sys
+
+
+# PART A
+x_i_raw = np.genfromtxt(sys.argv[1], delimiter=',')
+y_i = np.genfromtxt(sys.argv[2], delimiter=',')
+theta = np.array([0.00, 0.00])
+m = x_i_raw.size
+n = float(sys.argv[3])
+time_gap = float(sys.argv[4])
+
+# Normalize data
+mean = 0
+squared_sum = 0
+for xi in x_i_raw:
+    mean += xi
+    squared_sum += xi*xi
+mean = mean/m
+e_x_squared = squared_sum/m
+variance = e_x_squared - mean*mean
+
+x_i_norm = np.array([(xi-mean)/math.sqrt(variance) for xi in x_i_raw])
+x_i = np.array([[1, xi] for xi in x_i_norm])
+
+
+# Detect Convergence
+epsilon = 0.00000001
+def converged(theta_next, theta):
+    converged = True
+    for d in range(theta.size):
+        converged = converged and abs(theta_next[d] - theta[d]) < epsilon
+    return converged
+
+# Update Graph for PART C
+def update_lines(num, dataLines, lines):
+    for line, data in zip(lines, dataLines):
+        line.set_data(data[0:2, :num])
+        line.set_3d_properties(data[2, :num])
+    return lines
+
+def compute_error(theta_0, theta_1):
+    error = 0
+    for i in range(m):
+        hyp = theta_1*x_i[i][1] + theta_0
+        error += (y_i[i]-hyp)*(y_i[i]-hyp)
+    error = error/(2*m)
+    return error
+
+
+
+
+# Gradient Descent
+num_iterations = 0
+total_iterations = 10
+
+# Arrays ti hold parameter values for all iterations
+theta_0, theta_1, error_func = [], [], []
+curve = []
+
+while(True):
+    theta_next = np.array([0.00, 0.00])
+    j_theta = 0
+    for j in range(theta.size):
+        sum = 0
+        for i in range(m):
+            hyp = np.dot(theta, x_i[i])
+            sum = sum + x_i[i][j]*(y_i[i] - hyp)
+            j_theta = j_theta + (y_i[i] - hyp)*(y_i[i] - hyp)
+        theta_next[j] = theta[j] + n*sum/m
+        
+    if (converged(theta_next, theta)):
+        break
+
+    if (theta_next[1] - theta[1] > 1000000):
+        print ('Divergence observed in theta. Exiting...')
+        sys.exit()
+        
+    j_theta = j_theta/(4*m)
+    theta_0.append(theta[0])
+    theta_1.append(theta[1])
+    error_func.append(j_theta)
+    
+    theta = theta_next
+    num_iterations += 1
+    
+print (theta)
+print (num_iterations)
+
+
+
+
+# Plot Graphs for PART B
+h_theta = np.array([np.dot(theta, xi) for xi in x_i])
+plt.plot(x_i_raw, y_i, 'ro')
+plt.plot(x_i_raw, h_theta, 'b-')
+
+
+
+
+fig = plt.figure()
+ax = p3.Axes3D(fig)
+
+# Plot for PART C
+# Set Axes Properties
+theta_0_min, theta_0_max = -1, 3
+theta_1_min, theta_1_max = -2, 2
+error_func_min, error_func_max = 0, 5
+ax.set_xlim3d([theta_0_min, theta_0_max])
+ax.set_ylim3d([theta_1_min, theta_1_max])
+ax.set_zlim3d([error_func_min, error_func_max])
+
+# Make surface plot
+theta_0_surface = np.linspace(theta_0_min, theta_0_max, 300)
+theta_1_surface = np.linspace(theta_1_min, theta_1_max, 300)
+theta_0_surface, theta_1_surface = np.meshgrid(theta_0_surface, theta_1_surface)    
+error_func_surface = compute_error(theta_0_surface, theta_1_surface)
+
+# Plot the error function surface
+ax.plot_surface(theta_0_surface, theta_1_surface, error_func_surface, cmap = cm.coolwarm, 
+                linewidth=0, antialiased=False)
+
+# Plot error curve versus parameters
+curve = [np.array([theta_0, theta_1, error_func])]
+lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1], 'r.')[0] for dat in curve]
+ani = animation.FuncAnimation(fig, update_lines, num_iterations-1, fargs=(curve, lines), 
+                              interval=200, blit=True)
+plt.show()
+
+
+
+
+# Plot contour for PART D
+plt.ion()
+contour_plot = plt.plot([], [], 'r.')[0]
+contour = plt.contour(theta_0_surface, theta_1_surface, error_func_surface, [0.04, 0.16, 0.32, 0.64, 1.28, 2.00, 2.56])
+plt.clabel(contour, inline=1, fontsize=10)
+
+for i in range(num_iterations-1):
+    contour_plot.set_xdata(theta_0[:i])
+    contour_plot.set_ydata(theta_1[:i])
+    plt.pause(time_gap)
+    
+plt.show()
---
+++ assignment2/digit-classification.py
@@ -0,0 +1,192 @@
+import math
+import random
+import numpy as np
+import time
+import sys
+from cvxopt import matrix, solvers
+from cvxopt.solvers import qp
+import svmutil
+
+# Hyperparameters
+gamma = 0.05
+digit = 6
+
+# Inputs
+train_file = sys.argv[1]
+test_file = sys.argv[2]
+part = sys.argv[3]
+gaussian = part == 'b'
+
+
+# Get data for PART A
+train_data_raw = np.genfromtxt(train_file, delimiter=',')
+test_data_raw = np.genfromtxt(test_file, delimiter=',')
+
+
+
+# Process data to get the relevant vectors
+scale_down = lambda x : x/255
+train_data = np.array([np.zeros(shape=28*28 + 1)])
+test_data = np.array([np.zeros(shape=28*28 + 1)])
+
+for sample in train_data_raw:
+    if sample[-1] == digit or sample[-1] == digit+1:
+        insert_sample = np.array([scale_down(x) for x in sample[:-1]])
+        insert_sample = np.append(insert_sample, sample[-1])
+        train_data = np.vstack([train_data, insert_sample])
+        
+for sample in test_data_raw:
+    if sample[-1] == digit or sample[-1] == digit+1:
+        insert_sample = np.array([scale_down(x) for x in sample[:-1]])
+        insert_sample = np.append(insert_sample, sample[-1])
+        test_data = np.vstack([test_data, insert_sample])
+
+train_data = train_data[1:]
+test_data = test_data[1:]
+
+solvers.options['show_progress'] = False
+
+if part != 'c':
+    get_class = lambda x : 1 if x == digit else -1
+
+    # Global Parameters for our model
+    # m = 500
+    m = len(train_data)
+    alphas = np.array([])
+    w = np.array([])
+    b = 0
+    X = np.delete(train_data, -1, axis=1)
+    kernel = np.array([])
+
+
+    def get_parameters():
+        global m, alphas
+        # Get input for the solver
+        if not gaussian:
+            Y = np.diag([get_class(y) for y in train_data[:, -1]])
+            kernel = np.matmul(X, X.T)
+            temp_P = np.matmul(np.matmul(Y, kernel), Y)
+            P = matrix(temp_P)
+        else:
+            Y = np.diag([get_class(y) for y in train_data[:, -1]])
+            xtx = np.sum(np.multiply(X, X), 1).reshape(m, 1)
+            kernel_noexp = xtx + xtx.T - 2 * np.dot(X, X.T)
+            kernel = np.power(np.exp(-1*gamma), kernel_noexp)
+            temp_P = np.matmul(np.matmul(Y, kernel), Y)
+            P = matrix(temp_P)
+
+        q = matrix(1.0, (m,1))
+
+        G = matrix(np.identity(m))
+        G_identity = np.identity(m)
+        temp_G = np.concatenate((G_identity, -G_identity), axis=0)
+        G = matrix(temp_G)
+
+        h = matrix(0.0, (m,1))
+        h_zero = np.zeros(m)
+        h_ones = np.ones(m)
+        temp_h = np.append(h_zero, h_ones)
+        h = matrix(temp_h, (2*m,1))
+
+        temp_A = list(map(get_class, train_data[0:m, -1]))
+        A = matrix(np.array(temp_A), (1, m), 'd')
+
+        b = matrix(0.0)
+
+        # Use the cvxopt solver qp module
+        alphas = qp(P, q, G, h, A, b)['x']
+        alphas = np.array(-alphas)[:, 0]
+        
+    start_time = time.time()
+    get_parameters()
+    print ("Time Taken for evaluation of parameters: {0}".format(time.time()-start_time))
+
+    # Evaluate w if linear kernel used
+    if not gaussian:
+        w = np.zeros(28*28)
+        for i in range(m):
+            sample = train_data[i]
+            w += alphas[i] * get_class(sample[-1]) * sample[:-1]
+        # print (w)
+
+    # Evaluate b
+    # Find support vectors
+    support_vectors = []
+    epsilon = 0.0001
+    for i in range(m):
+        if alphas[i] > epsilon:
+            support_vectors.append(i)
+    print ("Number of Support Vectors: {0}".format(len(support_vectors)))
+
+    alpha_y = np.multiply(alphas, np.array(list(map(get_class, train_data[:, -1]))) )
+
+    maxone = -99999999
+    minone = 99999999
+    if not gaussian:
+        for sample in train_data[0:m]:
+            y = get_class(sample[-1])
+            if y == 1:
+                minone = min(minone, np.dot(w.T, sample[:-1]))
+            else:
+                maxone = max(maxone, np.dot(w.T, sample[:-1]))
+    else:    
+        w_trans_X = np.matmul(kernel, alpha_y)
+        for i in range(m):
+            wtx = w_trans_X[i]
+            y = get_class(train_data[i][-1])
+            if y == -1:
+                maxone = max(maxone, wtx)
+            else:
+                minone = min(minone, wtx)
+            
+    b = -(maxone + minone)/2
+
+    # Find accuracy
+    accuracy = 0
+    test_m = len(test_data)
+
+    for sample in test_data[0:test_m]:
+        if not gaussian:
+            pred_z = np.dot(w.T, sample[:-1]) + b
+            if pred_z > 0:
+                pred = 1
+            else:
+                pred = -1
+            if pred == get_class(sample[-1]):
+                accuracy += 1
+        else:
+            xtx = np.sum(np.multiply(sample[:-1], sample[:-1])).reshape(1,1)
+            XtX = np.sum(np.multiply(X, X), 1).reshape(m, 1)
+            inner_product = xtx + XtX.T - 2 * np.dot(sample[:-1], X.T)
+            wtx = np.dot(alpha_y, np.power(np.exp(-gamma), inner_product.T))
+            pred_z = wtx + b
+            if pred_z > 0:
+                pred = 1
+            else:
+                pred = -1
+            if pred == get_class(sample[-1]):
+                accuracy += 1
+
+    print ("Accuracy: {0}".format(accuracy/test_m * 100))
+
+else:
+    # Use libsvm
+    Y = train_data[:, -1]
+    X = np.delete(train_data, -1, axis=1)
+
+    start_time = time.time()
+    m_linear = svmutil.svm_train(Y, X, "-t 0 -c 1")
+    print ("Time taken by LibSVM for training Linear Kernels: {0}".format(time.time()-start_time))
+    print ("Number of Support Vectors for Linear Kernels by LibSVM: {0}".format(m_linear.get_nr_sv()))
+
+    start_time = time.time()
+    m_gaussian = svmutil.svm_train(Y, X, "-t 2 -c 1 -g 0.05")
+    print ("Time taken by LibSVM for training Gaussian Kernels: {0}".format(time.time()-start_time))
+    print ("Number of Support Vectors for Gaussian Kernels by LibSVM: {0}".format(m_gaussian.get_nr_sv()))
+
+    Y_test = test_data[:, -1]
+    X_test = np.delete(test_data, -1, axis=1)
+    print ("For Linear Kernels: ", end='')
+    labels_linear = svmutil.svm_predict(Y_test, X_test, m_linear)
+    print ("For Gaussian Kernels: ", end='')
+    labels_gaussian = svmutil.svm_predict(Y_test, X_test, m_gaussian)
